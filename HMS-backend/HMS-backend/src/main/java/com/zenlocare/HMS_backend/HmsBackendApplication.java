e5e0867dcf237413bdc76d66eed28528765e5cb8
KarthiZenoHosp <karthikeyan.m@zenohosp.com>
2026-03-20 14:24:22 +0530

    fix: add missing RestTemplate bean definition to resolve authService initialization error

--- HMS-backend/HMS-backend/src/main/java/com/zenlocare/HMS_backend/HmsBackendApplication.java
+++ HMS-backend/HMS-backend/src/main/java/com/zenlocare/HMS_backend/HmsBackendApplication.java
@@ -2,6 +2,8 @@ package com.zenlocare.HMS_backend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 public class HmsBackendApplication {
@@ -10,4 +12,8 @@ public class HmsBackendApplication {
 		SpringApplication.run(HmsBackendApplication.class, args);
 	}
 
+	@Bean
+	public RestTemplate restTemplate() {
+		return new RestTemplate();
+	}
 }
