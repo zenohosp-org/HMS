1df687a4374abc849a743d30b26f7caa06153fac
KarthiZenoHosp <karthikeyan.m@zenohosp.com>
2026-03-20 14:39:42 +0530

    fix: resolve RestTemplate bean conflict and add property defaults for robust startup

--- HMS-backend/HMS-backend/src/main/java/com/zenlocare/HMS_backend/HmsBackendApplication.java
+++ HMS-backend/HMS-backend/src/main/java/com/zenlocare/HMS_backend/HmsBackendApplication.java
@@ -2,8 +2,6 @@ package com.zenlocare.HMS_backend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 public class HmsBackendApplication {
@@ -12,8 +10,4 @@ public class HmsBackendApplication {
 		SpringApplication.run(HmsBackendApplication.class, args);
 	}
 
-	@Bean
-	public RestTemplate restTemplate() {
-		return new RestTemplate();
-	}
 }
